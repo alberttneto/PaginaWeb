68bd5f3aa53e44d77bcd055650ee552af613a168
alberttneto <bf__@hotmail.com>
2024-04-09 18:04:20 -0300

    Inserindo projetos

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,3 +1,4 @@
+import Card from "../Card";
 import "./Projects.css"
 
 const Projects = () => {
@@ -5,22 +6,26 @@ const Projects = () => {
     const projects = [
         {name: "Simulador MT", 
         description: "Ferramenta que simula uma máquina de Turing, um modelo teórico de computação. Com uma interface intuitiva, permite aos usuários criar e executar programas turing completos, visualizar o funcionamento da fita e acompanhar o estado interno da máquina. Ideal para o estudo e compreensão dos conceitos fundamentais da computação teórica.", 
-        image: "", tipo: "Acadêmico"},
+        image: "", type: "Acadêmico"},
         {name: "Replica Página Netflix", 
         description: "", 
-        image: "", tipo: "Cursos"},
+        image: "", type: "Cursos"},
         {name: "Calculadora", 
         description: "", 
-        image: "", tipo: "Cursos"},
+        image: "", type: "Cursos"},
         {name: "Camimica", 
         description: "", 
-        image: "", tipo: "Pessoal"},
+        image: "", type: "Pessoal"},
         {name: "Site Clínica Médica", 
         description: "",
-        image: "", tipo: "Acadêmico"},
-        {name: "", 
+        image: "", type: "Acadêmico"},
+        {name: "Organizador de Equipes", 
         description: "", 
-        image: "", tipo: ""}
+        image: "", type: ""},
+        {name: "Organizador de Equipes", 
+        description: "", 
+        image: "", type: ""}
+
     ];
 
     return (
@@ -39,6 +44,16 @@ const Projects = () => {
                     Cursos
                 </li>
             </ul>
+            <div className="cards">
+                {projects.map((project) => {
+                    return <Card 
+                        name={project.name}
+                        description={project.description}
+                        image={project.image}
+                        type={project.type}
+                    />
+                })}
+            </div>
 
         </div>
     );
